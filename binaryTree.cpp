171b564e72d0e0d8d100e6b33747c7b4378a1394
Ayush Anand <ayushanand273866@gmail.com>
2026-04-19 00:31:19 +0530

    binaryTree::stdBST

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -2,43 +2,48 @@
 #include "allocator.hpp"
 
 #include <functional>
-#include <map>
-#include <vector>
+#include <utility>
 #include <iostream>
 
 struct stdBST {
     struct MetaData {};
 
-
-    // trying to pass allocator, should memPool be passed or createNode, if memPool, how construct creteNode locally
-    template <typename Node, typename Type, typename Compare, typename Allocator>
-    static void __insert_node(Node **root, const Type& data, Compare comp, Allocator& memPool){
+    // returns ifInsertDone
+    template <typename Node, typename Compare>
+    static bool __insert_node(Node **root, Node *newNode, Compare comp){
         if(*root == nullptr){
-            *root = createNode(nullptr, nullptr, MetaData{}, data);
-            return;
+            *root = newNode;
+            return true;
         }
 
-        // return <Node *parent, bool isOnParentsRight>
-        auto __search_recur = [comp, data](auto& self, Node *node) -> std::pair<Node *, int> {
-            if(comp(data, node -> data)){
-                if(node -> left == nullptr) return {node, 0};
-                else return self(self, node -> left);
+        Node *curr = *root;
+        int isOnRight = 0;
+        while(1){
+            if(comp(newNode -> data, curr -> data)){
+                if(curr -> left == nullptr){
+                    isOnRight = 0;
+                    break;
+                }
+                else curr = curr -> left;
             }
-            else if(comp(node -> data, comp)){
-                if(node -> right == nullptr) return {node, 1};
-                else return self(self, node -> right);
+            else if(comp(curr -> data, newNode -> data)){
+                if(curr -> right == nullptr){
+                    isOnRight = 1;
+                    break;
+                }
+                else curr = curr -> right;
             }
-            else return {nullptr, 0};
-        };
+            else return false;
+        }
 
-        std::pair<Node *, bool> __search_res = __search_recur(__search_recur, *root);
-        if(__search_res.first == nullptr) return;
-        else __search_res.first -> ptr_arr[__search_res.second] = createNode(nullptr, nullptr, MetaData{}, data);
+
+        curr -> ptr_arr[isOnRight] = newNode;
+        return true;
     }
 
-    template<typename Node, typename Type, typename Compare, typename Deallocator>
-    static void __delete_node(Node **root, const Type& data, Compare comp, Deallocator& destroyNode){
-        if(*root == nullptr) return;
+    template <typename Node, typename Type, typename Compare>
+    static Node *__delete_node(Node **root, const Type& target, Compare comp){
+        if(*root == nullptr) return nullptr;
         
         // root exception for deletion, a dummy node for general logic
         Node root_parent(*root, nullptr, MetaData{}, lazyTypeDummy{});
@@ -46,12 +51,13 @@ struct stdBST {
         Node *parent = &root_parent, *curr = *root;
         int isRight = 0;
         while(1){
-            if(comp(data, curr -> data)){
+            if(curr == nullptr) return nullptr;
+            if(comp(target, curr -> data)){
                 parent = curr;
                 curr = curr -> left;
                 isRight = 0;
             }
-            else if(comp(curr -> data, data)){
+            else if(comp(curr -> data, target)){
                 parent = curr;
                 curr = curr -> right;
                 isRight = 1;
@@ -59,20 +65,48 @@ struct stdBST {
             else break;
         }
 
-        for(int i = 0;i < 2;i++) if(curr -> ptr_arr[i] == nullptr) {
-            parent -> ptr_arr[isRight] = curr -> ptr_arr[i];
-            return;
+        if(curr -> left == nullptr && curr -> right == nullptr){
+            parent -> ptr_arr[isRight] = nullptr;
+            *root = root_parent.left;
+            return curr;
         }
 
-        bool rng;
+        for(int i = 0;i < 2;i++) if(curr -> ptr_arr[i] == nullptr && curr -> ptr_arr[(i + 1) % 2] != nullptr) {
+            parent -> ptr_arr[isRight] = curr -> ptr_arr[(i + 1) % 2];
+            *root = root_parent.left;
+            return curr;
+        }
+
+        int rng = !isRight;
+        auto o = [rng](){
+            return (rng + 1) % 2;
+        };
+
         parent -> ptr_arr[isRight] = curr -> ptr_arr[rng];
         Node *curr1;
-        for(curr1 = curr -> ptr_arr[rng];curr1 -> ptr_arr[(rng + 1) % 2] != nullptr;curr1 = curr1 -> ptr_arr[(rng + 1) % 2]);
-        curr1 -> ptr_arr[(rng + 1) % 2] = curr -> ptr_arr[(rng + 1) % 2];
+        for(curr1 = curr -> ptr_arr[rng];curr1 -> ptr_arr[o()] != nullptr;curr1 = curr1 -> ptr_arr[o()]);
+        curr1 -> ptr_arr[o()] = curr -> ptr_arr[o()];
 
-        destroyNode(curr);
-        *root = root_parent -> left;
+        *root = root_parent.left;
+
+        return curr;
     }
+
+    template <typename Node, typename Type, typename Compare>
+    static bool __search_node(Node *root, const Type& target, Compare comp){
+        Node *curr = root;
+        while(curr != nullptr){
+            if(comp(target, curr -> data)) curr = curr -> left;
+            else if(comp(curr -> data, target)) curr = curr -> right;
+            else break;
+        }
+        return (curr != nullptr) ? true : false;
+    }
+};
+
+struct avlBST {
+    struct MetaData {int height;};
+
 };
 
 
@@ -90,7 +124,7 @@ class binaryTree {
             typename TreeType::MetaData meta;
 
             template <typename... Args>
-            Node(Node *l, Node *r, TreeType::MetaData m, Args&&... args) : left(l), right(r), meta(m), lazyTypeConstructor<Type> (std::forward<Args>(args)...) {};            
+            Node(Node *l, Node *r, typename TreeType::MetaData m, Args&&... args) : left(l), right(r), meta(m), lazyTypeConstructor<Type> (std::forward<Args>(args)...) {};            
         };
         
         segmentedArrayAllocator<Node> memPool;
@@ -99,10 +133,10 @@ class binaryTree {
         Compare comp;
 
         template <typename... Args>
-        Node *createNode(Node *left, Node *right, TreeType::Metadata& meta, Args&&... args){
+        Node *createNode(Node *left, Node *right, typename TreeType::MetaData meta, Args&&... args){
             Node *newNode = memPool.allocate();
             return new (newNode) Node(left, right, meta, std::forward<Args>(args)...);
-        }
+        };
 
         void destroyNode(Node *node){
             node -> data.~Type();
@@ -110,7 +144,6 @@ class binaryTree {
         }
 
     public :
-        
         binaryTree(size_t contiguousNodes = 256) : memPool(contiguousNodes), root(nullptr){
             this -> contiguousNodes = contiguousNodes;
         }
@@ -118,9 +151,9 @@ class binaryTree {
         ~binaryTree(){
             auto free_recur = [](auto& self, Node *node){
                 if(node == nullptr) return;
-                node -> data.~Type();
                 self(self, node -> left);
                 self(self, node -> right);
+                node -> data.~Type();
             };
 
             free_recur(free_recur, root);
@@ -130,29 +163,33 @@ class binaryTree {
             return (root == nullptr) ? 1 : 0;
         }
 
-        void insert(const Type& data){
-            TreeType::__insert_node<Node, Type, Compare, segmentedArrayAllocator<Type>> (&root, data, comp, memPool);
+        template <typename... Args>
+        void insert(Args&&... args){
+            Node *newNode = createNode(nullptr, nullptr, typename TreeType::MetaData {}, std::forward<Args>(args)...);
+
+            bool isInsertDone = TreeType::__insert_node(&root, newNode, comp);
+            if(!isInsertDone) destroyNode(newNode);
+        }
+
+        void erase(const Type& target){
+            Node *target_node = TreeType::__delete_node(&root, target, comp);
+
+            if(target_node != nullptr) destroyNode(target_node);
+        }
+
+        bool search(const Type& target){
+            return TreeType::__search_node(root, target, comp);
         }
         
         void printTree_side(std::ostream& os = std::cout){
             auto print = [&os](auto& self, Node *node, int depth){
                 if(node == nullptr) return;
-                self(self, node -> right, depth + 1, os);
-                for(int i = 0;i < depth;++i) os << '\t';
+                self(self, node -> right, depth + 1);
+                for(int i = 0;i < depth;++i) os << ' ';
                 os << node -> data << std::endl;
-                self(self, node -> left, depth + 1, os);
+                self(self, node -> left, depth + 1);
             };
 
             print(print, root, 0);
         }
-
-        // void printTree_vert(std::ostream& os){
-        //     std::vector<int> char_matrix;
-
-        //     auto idx = [&char_matrix]()
-
-        //     auto print = [&char_matrix](Node *node, int passedLeft, std::pair<int, int> pos){
-        //         if()
-        //     };
-        // }
 };
